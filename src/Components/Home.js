86fd88c7fa8315cec845505704402845c8492ade
sikku85 <sikku.sauravkbc@gmail.com>
2023-07-05 18:21:31 +0530

    image_reloution_changes

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -156,25 +156,26 @@ export const Home = () => {
 
               <div className="portfolio-grid grid">
                 <div>
-                  <img src={require("../images/1.1.jpg")} alt="" />
+                  <img src={require("../images/1.5.1 (1).jpg")} alt="" />
+                </div>
+
+                <div>
+                  <img src={require("../images/1.2.jpg")} alt="" />
                 </div>
                 <div>
-                  <img src={require("../images/1.2.JPG")} alt="" />
+                  <img src={require("../images/1.3.jpg")} alt="" />
                 </div>
                 <div>
-                  <img src={require("../images/1.3.JPG")} alt="" />
+                  <img src={require("../images/1.4.jpg")} alt="" />
                 </div>
                 <div>
-                  <img src={require("../images/1.4.JPG")} alt="" />
+                  <img src={require("../images/1.5.jpg")} alt="" />
                 </div>
                 <div>
-                  <img src={require("../images/1.5.JPG")} alt="" />
+                  <img src={require("../images/1.6.jpg")} alt="" />
                 </div>
                 <div>
-                  <img src={require("../images/1.6.JPG")} alt="" />
-                </div>
-                <div>
-                  <img src={require("../images/1.7.JPG")} alt="" />
+                  <img src={require("../images/1.7.jpg")} alt="" />
                 </div>
                 <div>
                   <img src={require("../images/1.8.JPG")} alt="" />
